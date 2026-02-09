4c04665e89a0a0b5a783482296d10bf5f1f388a5
c protko <protkoc02@gmail.com>
2026-02-08 23:01:00 -0500

    Update header.js

---
+++ little-mermaid/src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <Header>{
+            "The Little Mermaid by Hans Christian Andersen"
+        }
+        </Header>
+    );
+    }
+
